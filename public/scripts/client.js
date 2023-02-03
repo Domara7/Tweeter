4fb7755ef4f0a254b50ec7a67d1a9cf903b5c827
Domara Hawil <domara.hawil@hotmail.com>
2023-02-02 19:51:33 -0500

    creating two functions, one to generate the DOM structure for a tweet and one for appending each of the tweets in our first function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,64 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+$(document).ready(function () {
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
+      },
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
+      },
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
+    },
+  ];
+
+  const renderTweets = (tweets) => {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      return $("#tweets-container").append($tweet);
+    }
+  };
+
+  const createTweetElement = (data) => {
+    return `
+        <article>
+          <header class="profile">
+            <div class="picture-name">
+              <img src=${data.user.avatars} />
+              <p>${data.user.name}</p>
+            </div>
+            <div class="name-handle">${data.user.handle}</div>
+          </header>
+          <p class="tweet-body">
+            ${data.content.text}
+          </p>
+          <footer class="footer">
+            <div class="days">${data.created_at}</div>
+            <div class="icons">
+              <i class="fa-solid fa-flag"></i>
+              <i class="fa-solid fa-retweet"></i>
+              <i class="fa-solid fa-heart"></i>
+            </div>
+          </footer>
+        </article>
+  `;
+  };
+
+  renderTweets(data);
+});
